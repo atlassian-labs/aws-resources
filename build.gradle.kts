35475565a7279249908c690fad8625735d09526a
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2018-08-03 11:10:17 +0200

    JPT-669 Configure Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,21 @@
-plugins {
-    kotlin("jvm").version(Versions.kotlin)
-    maven
-}
+val kotlinVersion = "1.2.30"
 
-maven{
-    group = "com.atlassian.test.performance"
-    version = "0.0.1-SNAPSHOT"
+plugins {
+    kotlin("jvm").version("1.2.30")
+    id("com.atlassian.performance.tools.gradle-release").version("0.0.2")
 }
 
 dependencies {
-    compile(Libs.concurrency)
-    compile(Libs.io)
-    compile(Libs.ssh)
+    compile("com.atlassian.performance.tools:concurrency:0.0.1")
+    compile("com.atlassian.performance.tools:io:0.0.1")
+    compile("com.atlassian.performance.tools:ssh:0.1.0")
 
-    compile(Libs.junit)
-    compile(Libs.hamcrest)
-    compile(Libs.kotlinStandard)
-    compile(Libs.jcip)
-    compile(Libs.commonsCodec)
-    Libs.aws(
+    compile("junit:junit:4.12")
+    compile("org.hamcrest:hamcrest-library:1.3")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
+    compile("net.jcip:jcip-annotations:1.0")
+    compile("commons-codec:commons-codec:1.10")
+    aws(
         "ec2",
         "s3",
         "iam",
@@ -28,5 +25,35 @@ dependencies {
         "support"
     ).forEach { compile(it) }
 
-    Libs.log4jCore().forEach { compile(it) }
+    log4jCore().forEach { compile(it) }
 }
+
+fun aws(
+    vararg modules: String
+): List<String> = modules
+    .map { module ->
+        "com.amazonaws:aws-java-sdk-$module:1.11.298"
+    }
+    .plus(log4j("jcl"))
+    .plus(jaxb())
+
+fun log4jCore(): List<String> = log4j(
+    "api",
+    "core",
+    "slf4j-impl"
+)
+
+fun log4j(
+    vararg modules: String
+): List<String> = modules.map { module ->
+    "org.apache.logging.log4j:log4j-$module:2.10.0"
+}
+
+fun jaxb(): List<String> = listOf(
+    "org.glassfish.jaxb:jaxb-runtime:2.3.0",
+    "javax.activation:activation:1.1.1"
+)
+
+val wrapper = tasks["wrapper"] as Wrapper
+wrapper.gradleVersion = "4.9"
+wrapper.distributionType = Wrapper.DistributionType.ALL
