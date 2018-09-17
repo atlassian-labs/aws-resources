06a8f2a352e8417a25599eb694bc6c2941ea3c0e
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2018-09-17 17:04:46 +0200

    JPERF-60: Reproduce the hang

---
+++ src/test/kotlin/com/atlassian/performance/tools/aws/api/StackFormulaTest.kt
@@ -0,0 +1,42 @@
+package com.atlassian.performance.tools.aws.api
+
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.regions.Regions
+import com.atlassian.performance.tools.aws.FakeAwsCredentialsProvider
+import org.hamcrest.CoreMatchers.notNullValue
+import org.junit.Assert.assertThat
+import org.junit.Test
+import java.time.Duration
+
+class StackFormulaTest {
+    private class MissingCredentialsProvider : AWSCredentialsProvider by FakeAwsCredentialsProvider() {
+        override fun getCredentials(): AWSCredentials {
+            throw Exception("Credentials are missing")
+        }
+    }
+
+    @Test(timeout = 5000)
+    fun shouldNotHang() {
+        val formula = StackFormula(
+            investment = Investment(
+                useCase = "Unit test the StackFormula",
+                lifespan = Duration.ofMinutes(1)
+            ),
+            aws = Aws(
+                region = Regions.DEFAULT_REGION,
+                credentialsProvider = MissingCredentialsProvider()
+            ),
+            cloudformationTemplate = "malformed YAML content"
+        )
+
+        val exception: Exception? = try {
+            formula.provision()
+            null
+        } catch (e: Exception) {
+            e
+        }
+
+        assertThat("should fail for some reason", exception, notNullValue())
+    }
+}
