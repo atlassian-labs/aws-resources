2b44e6a8275f0c2fc470ccd34d7fbfb71b5277c7
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-02-21 18:44:04 +0100

    Enable Gradle build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "aws-resources"
+
+plugins {
+    id("com.gradle.enterprise").version("3.12.3")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
