7b9c5c6ba1c6d5eccd5e163cddeb8fb81089b521
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-08-14 12:12:11 +0200

    JPERF-1272: Refactor StorageIT
    TDD: refactor

--- src/test/kotlin/com/atlassian/performance/tools/aws/api/StorageIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/aws/api/StorageIT.kt
@@ -1,6 +1,6 @@
 package com.atlassian.performance.tools.aws.api
 
-import com.atlassian.performance.tools.aws.IntegrationTestRuntime
+import com.atlassian.performance.tools.aws.IntegrationTestRuntime.aws
 import com.atlassian.performance.tools.io.api.ensureDirectory
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 import java.nio.file.Path
-import java.util.*
+import java.util.UUID.randomUUID
 
 class StorageIT {
 
@@ -17,70 +17,82 @@ class StorageIT {
 
     @Test
     fun shouldUploadAndDownload(@TempDir folder: Path) {
+        // given
         val expectedFileName = "test-artifact.txt"
-        val expectedFile = File(javaClass.getResource(expectedFileName).toURI())
+        val expectedFile = File(javaClass.getResource(expectedFileName)!!.toURI())
         val expectedFileContent = expectedFile.readText()
-        val nonce = "StorageTest-${UUID.randomUUID()}"
+        val nonce = "StorageTest-${randomUUID()}"
         logger.info("Generated nonce for the test: $nonce.")
-        val storage = IntegrationTestRuntime.aws.resultsStorage(nonce)
+        val storage = aws.resultsStorage(nonce)
 
+        // when
         storage.upload(expectedFile)
         val actualFile = storage.download(folder).resolve(expectedFileName).toFile()
         val actualFileContent = actualFile.readText()
 
+        // then
         assertThat(actualFileContent).isEqualTo(expectedFileContent)
     }
 
     @Test
     fun shouldUploadAndDownloadFileWithSpecialCharacters(@TempDir folder: Path) {
+        // given
         val expectedFileName = "test-artifact ::.txt"
-        val expectedFile = File(javaClass.getResource(expectedFileName).toURI())
+        val expectedFile = File(javaClass.getResource(expectedFileName)!!.toURI())
         val expectedFileContent = expectedFile.readText()
-        val nonce = "StorageTest%% : : -${UUID.randomUUID()}"
+        val nonce = "StorageTest%% : : -${randomUUID()}"
         logger.info("Generated nonce for the test: $nonce.")
-        val storage = IntegrationTestRuntime.aws.resultsStorage(nonce)
+        val storage = aws.resultsStorage(nonce)
 
+        // when
         storage.upload(expectedFile)
         val actualFile = storage.download(folder).resolve(expectedFileName).toFile()
         val actualFileContent = actualFile.readText()
 
+        // then
         assertThat(actualFileContent).isEqualTo(expectedFileContent)
     }
 
     @Test
     fun shouldUploadAndDownloadSymmetrically(@TempDir folder: Path) {
+        // given
         val upload = folder.resolve("upload").ensureDirectory()
         val download = folder.resolve("download").ensureDirectory()
         val subfolder = upload.resolve("subfolder").ensureDirectory()
         val file = subfolder.resolve("testfile.txt")
         file.toFile().createNewFile()
-        val nonce = "StorageTest.shouldUploadAndDownloadSymmetrically.${UUID.randomUUID()}"
+        val nonce = "StorageTest.shouldUploadAndDownloadSymmetrically.${randomUUID()}"
 
-        val storage = IntegrationTestRuntime.aws.resultsStorage(nonce)
+        // when
+        val storage = aws.resultsStorage(nonce)
         storage.upload(upload.toFile())
         storage.download(download).toFile()
 
+        // then
         assertThat(download).exists()
         assertThat(download.toFile().list()).isEqualTo(upload.toFile().list())
     }
 
     @Test
     fun shouldBeAbleToCacheResults(@TempDir folder: Path) {
+        // given
         val upload = folder.resolve("upload").ensureDirectory()
         val download = folder.resolve("download").ensureDirectory()
         val cache = folder.resolve("cache").ensureDirectory()
         val file = upload.resolve("testfile.txt").toFile()
         file.createNewFile()
-        val nonce = "StorageTest.shouldBeAbleToCacheResults.${UUID.randomUUID()}"
-        val storage = IntegrationTestRuntime.aws.resultsStorage(nonce)
+        val nonce = "StorageTest.shouldBeAbleToCacheResults.${randomUUID()}"
+        val storage = aws.resultsStorage(nonce)
         storage.upload(upload.toFile())
         storage.download(download).toFile()
 
-        val cacheNonce = "StorageTest.shouldBeAbleToCacheResults.${UUID.randomUUID()}"
-        val cacheStorage = IntegrationTestRuntime.aws.resultsStorage(cacheNonce)
+        // when
+        val cacheNonce = "StorageTest.shouldBeAbleToCacheResults.${randomUUID()}"
+        val cacheStorage = aws.resultsStorage(cacheNonce)
         cacheStorage.upload(download.toFile())
         val cachedResults = cacheStorage.download(cache).toFile()
 
+        // then
         assertThat(cachedResults).exists()
         assertThat(cachedResults.list()).isEqualTo(upload.toFile().list())
         assertThat(cachedResults.list()).isEqualTo(download.toFile().list())
@@ -88,14 +100,15 @@ class StorageIT {
 
     @Test
     fun shouldNotMixPrefixes(@TempDir folder: Path) {
+        // given
         val uploadAlpha = folder.resolve("uploadAlpha").ensureDirectory()
         val uploadBeta = folder.resolve("uploadBeta").ensureDirectory()
         val downloadAlpha = folder.resolve("downloadAlpha").ensureDirectory()
         uploadAlpha.resolve("alpha.txt").toFile().also { it.createNewFile() }
         uploadBeta.resolve("beta.txt").toFile().also { it.createNewFile() }
-        val nonce = "shouldNotMixPrefixes-${UUID.randomUUID()}"
-        val storageAlpha = IntegrationTestRuntime.aws.resultsStorage("$nonce/sample-1")
-        val storageBeta = IntegrationTestRuntime.aws.resultsStorage("$nonce/sample-10")
+        val nonce = "shouldNotMixPrefixes-${randomUUID()}"
+        val storageAlpha = aws.resultsStorage("$nonce/sample-1")
+        val storageBeta = aws.resultsStorage("$nonce/sample-10")
 
         // when
         storageAlpha.upload(uploadAlpha.toFile())
